7cdd69932a128a2c1ad20bf0488dce19f194f932
Aguru itabashi <itabashi.a@gmail.com>
2023-10-14 15:58:43 +0900

    cognitoでの認証機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import HomePage from "./components/HomePage";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {
+  withAuthenticator,
+  Button,
+  useAuthenticator,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+Amplify.configure(awsExports);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 function App() {
+  const { signOut } = useAuthenticator();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar bg="primary" expand="lg" variant="dark">
+          <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Button onClick={signOut}>Sign out</Button>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        {/* <Route path="/uploadImage" exact component={UploadImage} /> */}
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
